433c9f958ca75ebc6fd218ba921e6790c7a53bc5
princemahesh <princejagdishrohara@gmail.com>
2023-06-09 23:04:43 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ server.listen(8080, () => {
   console.log('Server running on http://localhost:8080/');
 });
 
-const MONGO_URL = ''; // DB URI
+const MONGO_URL = 'mongodb+srv://princejagdishrohara:princemahesh@cluster0.2mpeuah.mongodb.net/'; // DB URI
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
